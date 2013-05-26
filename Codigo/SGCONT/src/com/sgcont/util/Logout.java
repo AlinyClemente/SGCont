0c5a3fbcfd5d8c43b7862edd42478c7f696dd63b
viniciusmmedeiros <viniciusmmedeiros@gmail.com>
2013-05-26 15:24:55 +0000

    RM 67 - Adição da apresentação do Nome do Usuário e da opção de Sair.

---
+++ Codigo/SGCONT/src/com/sgcont/util/Logout.java
@@ -0,0 +1,23 @@
+package com.sgcont.util;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+@ManagedBean (name="LogoutBean")
+@SessionScoped
+public class Logout implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+    public String efetuarLogout(){  
+        FacesContext fc = FacesContext.getCurrentInstance();  
+        HttpSession session = (HttpSession) fc.getExternalContext().getSession(false);  
+        session.invalidate();
+        return "login.xhtml?faces-redirect=true";
+
+    }  
+}
