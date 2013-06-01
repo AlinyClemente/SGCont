78f4a0c2c67c476a44b00290ac50acf78059c23f
viviannesousa <viviannesousa@gmail.com>
2013-06-01 19:15:47 +0000

    RM87 - Validar todos os campos inteiros do sistema para não permitir colocar letras

--- Codigo/SGCONT/WebContent/resources/javascript/util.js
+++ Codigo/SGCONT/WebContent/resources/javascript/util.js
@@ -3,3 +3,13 @@ function formatarCampoNumerico(z){
 	v = v.replace(/\D/g,"");  
 	z.value = v;
 }	
+
+function moeda(z){  
+	v = z.value;
+	v = v.replace(/\D/g,"");  
+	v = v.replace(/[0-9]{12}/,""); 
+	v = v.replace(/(\d{1})(\d{8})$/,"$1.$2");
+	v = v.replace(/(\d{1})(\d{5})$/,"$1.$2");
+	v = v.replace(/(\d{1})(\d{1,2})$/,"$1,$2");
+	z.value = v;
+}
